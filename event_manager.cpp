e3198979125731300a07a093134beb50442f70df
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-08-10 00:23:38 +0800

    event listener class

--- event_manager.cpp
+++ event_manager.cpp
@@ -4,8 +4,9 @@
 #include <algorithm>
 #include <vector>
 #include <map>
+#include <string>
 
-class EvntManager
+class EventManager
 {
 private :
     std::map<std::string, std::vector<void(*)(int)> > events;
@@ -15,8 +16,31 @@ public :
     EventManager(){}
     EventManager* on(std::string event_name, void(*callback)(int))
     {
+        std::vector<void(*)(int)> *listeners = &events[event_name];
+        if(std::find(listeners->begin(), listeners->end(), callback)!=listeners->end())
+        {
+            return this;
+        }
 
+        listeners->push_back(callback);
+        return this;
     }
-}
+
+    bool emit (std::string event_name, int arg)
+    {
+        std::vector<void(*)(int)>listeners = events[event_name];
+        if(listeners.size()==0)
+            return false;
+
+        for (int idx = 0;idx<listeners.size();idx++)
+        {
+            listeners[idx](arg);
+        }
+        return true;
+    }
+
+};
+
+
 
 #endif
