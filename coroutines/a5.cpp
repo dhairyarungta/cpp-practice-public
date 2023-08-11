6cdbb7ee8fd1361bacd59ae75bf0a655dc277970
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-08-11 11:47:01 +0800

    update

--- coroutines/a5.cpp
+++ coroutines/a5.cpp
@@ -42,3 +42,73 @@ void main()
     }
     h.destroy();
 }
+
+
+
+//the coyield essentially repalces the code below for the need of the GetPromise class
+//both the codes have the same functionality 
+
+
+
+template<typename PromiseType>
+struct GetPromise
+{   
+    PromiseType* p_;
+    bool await_ready(){return false;}
+    bool await_suspend(std::coroutine_handle<PromiseType>h)
+    {
+        p_ = &(h.promise());
+        return false;
+    }
+    PromiseType* await_resume (){return p_;}
+    
+};
+
+
+struct ReturnObject
+{
+    struct promise_type 
+    {
+        unsigned value_;
+        std::suspend_never initial_suspend(){return{};}
+        std::suspend_never final_suspend(){return{};}
+        void unhandled_exception(){}
+        ReturnObject get_return_value()
+        {
+            return {.h_ = std::coroutine_handle<promise_type>::from_promise(*this)};
+        }
+
+
+
+    };
+
+    std::coroutine_handle<promise_type> h_;
+    operator std::coroutine_handle<promise_type>()const {return h_;}
+
+};
+
+
+ReturnObject counter ()
+{
+    auto pp = co_await GetPromise<ReturnObject::proimse_type> {}; 
+    for (int i = 0;;i++)
+    {
+        pp->value_ = i;
+        co_await std::suspend_always{};
+        
+    }
+
+}
+
+void main()
+{
+    std::coroutine_handle<ReturnObject3::promise_type>h = counter3();
+    ReturnObject3::promise_type &promise =h.promise();
+    for (int i =0 ;i<3;i++)
+    {
+        std::cout<<"counter: "<<promsie.value_<<std::endl;
+        h();
+    }
+
+    h.destroy();
+}
