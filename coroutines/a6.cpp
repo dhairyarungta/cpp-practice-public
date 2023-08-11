2651dc2578cf67654cad0fea5f78701ce458e10d
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-08-11 12:17:43 +0800

    update

--- coroutines/a6.cpp
+++ coroutines/a6.cpp
@@ -3,10 +3,38 @@ struct ReturnObject
     struct promise_type
     {
         unsigned int value_;
+        ~promise_type()
+        {
+            std::cout<<"promise_type destroyed "<<std::endl; 
+        }
+        ReturnObject get_return_value() 
+        {
+            return {
+                .h_ = std::coroutine_handle<promise_type>::from_promise(*this)
+            };
+        }
+        std::suspend_never initial_suspend(){return {};}
+        std::suspend_never final_suspend(){return {};}
+        void unhadnled_exception(){}
+        void return_void(){}
+        std::suspend_always yiled_value(unsigned int value)
+        {
+            value_ = value;
+            return {};
+        }
         
     };
+
+    std::coroutine_handle<promise_type> h_;
 };
 
+ReturnObject counter()
+{
+    for (int i =0 ;i<3;i++)
+    {
+        co_yield i;
+    }
+}
 
 
 void main()
@@ -15,7 +43,8 @@ void main()
     auto & promise = h.promise();
     while(!h.done())
     {
-
+        std::cout<<"coutner: "<<promise.value_<<std::endl;
+        h();
     }
-
-}
+    h.destroy();
+}   
